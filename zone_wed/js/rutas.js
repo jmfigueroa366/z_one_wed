0f6fa61581d79d1632cfb87e2d52e32d9e680342
AndreaCarolina <acarolinaflorez@unicesar.edu.co>
2026-09-21 12:12:31 -0500

    Implementación de rutas.js

---
+++ zone_wed/js/rutas.js
@@ -0,0 +1,9 @@
+export const RUTAS = {
+    LOGIN: '/html/login.html',
+    MENU_PRINCIPAL: '/html/menu_principal.html',
+    INDEX: '/html/index.html',
+
+    PANEL_ADMIN: '/panel-admin/dashboard.html',
+    PANEL_COLABORADOR: '/panel-colaborador/dashboard.html',
+    PANEL_CLIENTE: '/panel-cliente/dashboard.html',
+};
